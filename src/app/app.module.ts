dc1a9c24c0b1b6871ad2a543401c0c9b93ef42b7
nahuelba <nahuelbarreiro@gmail.com>
2021-06-18 10:48:44 -0300

    simula tu credito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SimulaTuCreditoComponent } from './simula-tu-credito/simula-tu-credito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulaTuCreditoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
